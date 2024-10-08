659b041220cadcb265cfa00aa33927bfe1ef7d6a
starlash7 <yang92sw@gmail.com>
2024-10-08 13:44:15 +0900

    상태관리와 props1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useRef, useState, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+
+  }
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return <div className="App">
+    <h1>Todo</h1>
+    <Editor onClickAdd={onClickAdd} />
+
+  </div>;
 }
 
 export default App;
